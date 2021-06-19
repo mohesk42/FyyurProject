a66b83c9668218a1a4f21438af8104c25ca08db1
Mohammad Muneer <mohammadesk42@gmail.com>
2021-06-19 21:21:52 +0300

    app.py display venues (note)
    num_upcoming_shows not ready

--- app.py
+++ app.py
@@ -88,29 +88,37 @@ def index():
 @app.route('/venues')
 def venues():
   # TODO: replace with real venues data.
-  
+  data = []
+  for area in db.session.query(Venue.city, Venue.state).distinct():
+    area_dict = {
+      "city": area[0],
+      "state": area[1],
+      "venues": Venue.query.filter_by(city=area[0], state=area[1]).all()
+    }
+    data.append(area_dict)
+
   #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-      "id": 1,
-      "name": "The Musical Hop",
-      "num_upcoming_shows": 0,
-    }, {
-      "id": 3,
-      "name": "Park Square Live Music & Coffee",
-      "num_upcoming_shows": 1,
-    }]
-  }, {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }]
+  # data=[{
+  #   "city": "San Francisco",
+  #   "state": "CA",
+  #   "venues": [{
+  #     "id": 1,
+  #     "name": "The Musical Hop",
+  #     "num_upcoming_shows": 0,
+  #   }, {
+  #     "id": 3,
+  #     "name": "Park Square Live Music & Coffee",
+  #     "num_upcoming_shows": 1,
+  #   }]
+  # }, {
+  #   "city": "New York",
+  #   "state": "NY",
+  #   "venues": [{
+  #     "id": 2,
+  #     "name": "The Dueling Pianos Bar",
+  #     "num_upcoming_shows": 0,
+  #   }]
+  # }]
   return render_template('pages/venues.html', areas=data);
 
 @app.route('/venues/search', methods=['POST'])
